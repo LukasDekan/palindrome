430e2bfeda1f890b8f5e065482d22327ca8681dd
LukasDekan <Lukas.dekan24@student.cart.org>
2025-04-09 13:32:09 -0700

    Assignment complete

---
+++ index.js
@@ -0,0 +1,16 @@
+// COPY THIS CODE INTO THE CONSOLE
+function reverseWord(word) {
+    return word.split('').reverse().join('');
+  }
+  
+  let myWord = prompt("Type in a word");
+  let reversedWord = reverseWord(myWord);
+  if (myWord == reversedWord){
+    console.log(myWord + " is a palindrome! YAY!");
+  }
+  else if (myWord != reversedWord){
+    console.log(myWord + " is NOT a palindrome! Try again.");
+  }
+
+
+
